915a7981e3158ca5fcd9903e89368691b7aa50e7
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2025-04-18 03:24:34 +0530

    audio check

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //game variables and constants
 let inputDir={x:0,y:0};
-const foodSound=new Audio("food.mp3");
-const gameOverSound=new Audio("gameover.mp3");
-const moveSound=new Audio("move.mp3");
-const musicSound=new Audio("music.mp3");
-// const foodSound = document.getElementById('foodSound');
-// const gameOverSound = document.getElementById('gameOverSound');
-// const moveSound = document.getElementById('moveSound');
-// const musicSound = document.getElementById('musicSound');
+// const foodSound=new Audio("food.mp3");
+// const gameOverSound=new Audio("gameover.mp3");
+// const moveSound=new Audio("move.mp3");
+// const musicSound=new Audio("music.mp3");
+const foodSound = document.getElementById('foodSound');
+const gameOverSound = document.getElementById('gameOverSound');
+const moveSound = document.getElementById('moveSound');
+const musicSound = document.getElementById('musicSound');
 let score=0;
 
 const board = document.getElementById("board");
